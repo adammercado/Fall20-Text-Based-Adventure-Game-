475405e69be0ddec67200f494f0d003c78379993
michaeldton <tonm@oregonstate.edu>
2020-10-28 15:46:08 -0700

    changed comments

--- Inventory/inventory.py
+++ Inventory/inventory.py
@@ -3,17 +3,18 @@ from Item.item import Item
 
 """
 Methods:
-    display_backpack_menu()
-    show()
-    grab()
-    drop() 
+    __init__(): default constructor initializes empty list
+    displayInventory(): prints name and descrption of each item in inventory list
+    getInventoryList(): returns list attribute (used for json)
+    addItem(itemObj): receives an object of Item class and appends to list
+    removeItem(itemName): locates item with matching name in list and deletes from list - UNTESTED
 """
 
 class Inventory:
     def __init__(self):
         self.inventory = []
 
-# Show commands iterates through the list and prints the contents
+    # Iterate through list and print name and description of each item object 
     def displayInventory(self):
         for item in self.inventory:
             print(item.name)
@@ -22,11 +23,11 @@ class Inventory:
     def getInventoryList(self):
         return self.inventory
 
-# Add items to the backpack by appending list
+    # Add items to inventory by appending object to list
     def addItem(self, itemObj):
         self.inventory.append(itemObj)
 
-# Remove items from the backpack using "drop" command.
+    # Remove items - UNTESTED (parser does not handle remove commands yet)
     def removeItem(self, itemName):
         for i, o in enumerate(self.inventory):
             if o.name == itemName:
