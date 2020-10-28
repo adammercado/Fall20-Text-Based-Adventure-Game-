df0570ae78fcffd6cc4fe00542698611a0fb359e
michaeldton <tonm@oregonstate.edu>
2020-10-28 14:53:27 -0700

    added methods and constructor to inventory

--- Inventory/inventory.py
+++ Inventory/inventory.py
@@ -10,25 +10,22 @@ Methods:
 """
 
 class Inventory:
-    backpack_list = ["Tome", "Walking Stick"]
-
-# These are the commands that can be used to look at and manipulate objects in the backpack
+    def __init__(self):
+        self.inventory = []
 
 # Show commands iterates through the list and prints the contents
-    def show(self):
-        for (i, item) in enumerate(backpack_list):
-            print(i, item)
-        print()
+    def displayInventory(self):
+        for item in self.inventory:
+            print(item.name)
 
 # Add items to the backpack by appending list
-    def grab(self):
-        item = input("Name: ")
-        backpack_list.append(item)
-        print(item + " was added to your backpack.\n")
+    def addItem(self, itemObj):
+        self.inventory.append(itemObj)
 
 # Remove items from the backpack using "drop" command.
-    def drop(self):
-        number = int(input("Which Item Number do you want to drop?: "))
-        orig_inp = backpack_list[number]
-        del backpack_list[number]
-        print("'{}' was deleted.\n".format(orig_inp))
+    def removeItem(self, itemName):
+        for i, o in enumerate(self.inventory):
+            if o.name == itemName:
+                del self.inventory[i]
+                break
+                      
