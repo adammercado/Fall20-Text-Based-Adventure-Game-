c3b16cc70d540c1ffa32851e8210dd7ff0b76ce7
michaeldton <tonm@oregonstate.edu>
2020-11-14 12:17:45 -0800

    initialize constructor and test method for Feature

---
+++ Feature/feature.py
@@ -0,0 +1,11 @@
+class Feature:
+
+    def __init__(self, name, desc, isInteractive, interactions):
+        self.name = name
+        self.desc = desc
+        self.isInteractive = isInteractive
+        self.interactions = interactions
+
+    def getInfo(self):
+        print("Feature: {}\nDesc: {}\nInteract: {}\nInteract With: {}".format(self.name, self.desc, self.isInteractive, self.interactions))
+
