239c7025c9eb903291cc7e1d90665f88f2f571a9
michaeldton <tonm@oregonstate.edu>
2020-11-25 15:18:26 -0600

    Fixed issues with item names not being use correctly to add/drop items in interactions

--- Progression/progression.py
+++ Progression/progression.py
@@ -1,32 +1,23 @@
-from Room.room import Room
-from Player.player import Player
-from Inventory.inventory import Inventory
-from Item.item import Item
-import re
-
 
 class Progression:
     def __init__(self):
         self.interactions = [
-            ("Tree Branch", "Serene Forest - North", "Rusted Key"),
-            ("Rusted Key", "Abandoned Home", "Dull Pendant")
+            ("tree branch", "Serene Forest - North", "rusted key"),
+            ("rusted key", "Abandoned Home", "dull pendant")
         ]
 
     def perform_interaction(self, item, player_inventory, room):
         pair = (item, room.name)
 
         for group in self.interactions:
-            if pair[0] == group[0].lower() and pair[1] == group[1]:
-                # Debug print statements
-                print("Conditional check match passed.")
-                print("Item to be dropped: {}".format(group[2]))
-
+            if pair[0] == group[0] and pair[1] == group[1]:
                 for obj in room.inventory.get_inventory_list():
                     if obj.name.lower() == group[2]:
+                        print("Item to be dropped: {}".format(group[2]))
                         obj.toggle_obtainable()
                         room.room_drop_item(group[2], player_inventory)
+                        print("End of function.")
                         break
-
-                return
+                break
             else:
                 print("Not a valid interaction.")
